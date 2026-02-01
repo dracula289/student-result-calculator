000c7fa5a27812a62eb42e2b64a7b3a5fb0076c2
adharsh28_ <adharshx510@gmail.com>
2026-02-02 00:37:54 +0530

    Add files via upload

---
+++ student result calci/calc.css
@@ -0,0 +1,24 @@
+
+function calculateResult() {
+
+    var beee = Number(document.getElementById("BEEE").value);
+    var de   = Number(document.getElementById("DE").value);
+    var ed   = Number(document.getElementById("ED").value);
+    var pt   = Number(document.getElementById("PT").value);
+    var os   = Number(document.getElementById("OS").value);
+
+    var total = beee + de + ed + pt + os;
+    var percentage = total / 5;
+
+    var grade;
+    if (percentage >= 50) {
+        grade = "PASS";
+    } else {
+        grade = "FAIL";
+    }
+
+    document.getElementById("result").innerHTML =
+        "Total Marks: " + total +
+        "<br>Percentage: " + percentage + "%" +
+        "<br>Result: " + grade;
+}
---
+++ student result calci/readme.md
@@ -0,0 +1,25 @@
+# Student Result Calculator
+
+A simple web-based mini project to calculate student total marks, percentage, and result.
+
+## Technologies Used
+- HTML
+- CSS
+- JavaScript
+
+## Features
+- Enter marks for multiple subjects
+- Calculates total marks
+- Calculates percentage
+- Displays pass or fail result
+
+## How to Run
+1. Download or clone the repository
+2. Open `index.html` in a web browser
+3. Enter marks and click Calculate
+
+## Output
+The application displays total marks, percentage, and result instantly.
+
+## Author
+Adhik
